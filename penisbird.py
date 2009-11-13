10bcff927d2ded3f73202d1b9936093c89afd3ce
William Pitcock <nenolod@atheme.org>
2009-11-12 20:51:33 -0600

    lols.

--- penisbird.py
+++ penisbird.py
@@ -4,8 +4,9 @@ import asyncore
 import penis.ircsocket
 from penis.protocol import rfc1459_client
 
-sb = penis.ircsocket.ircconn('irc.systeminplace.net', 6667, 'rxmirror', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#rapidxen' }, friendlyname='staticbox')
-sb2 = penis.ircsocket.ircconn('irc.freenode.net', 6667, 'rxmirror', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#rapidxen' }, friendlyname='freenode')
+penis.ircsocket.ircconn('irc.systeminplace.net', 6667, 'rxmirror', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#rapidxen' }, friendlyname='staticbox')
+penis.ircsocket.ircconn('irc.freenode.net', 6667, 'rxmirror', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#rapidxen' }, friendlyname='freenode')
+penis.ircsocket.ircconn('irc.efnet.net', 6667, 'rxmirror', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#rapidxen' }, friendlyname='efnet')
 
 asyncore.loop()
 
