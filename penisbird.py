83be59a0c8681cf25f32a92fab8da548308352f7
JStoker <jstoker@jcs.me.uk>
2010-10-24 21:15:38 +0100

    Modified the default networks/channels the bot joins.

--- penisbird.py
+++ penisbird.py
@@ -4,9 +4,8 @@ import asyncore
 import penis.ircsocket
 from penis.protocol import rfc1459_client
 
-penis.ircsocket.ircconn('irc.systeminplace.net', 6667, 'rxmirror', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#rapidxen' }, friendlyname='staticbox')
-penis.ircsocket.ircconn('irc.freenode.net', 6667, 'rxmirror', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#rapidxen' }, friendlyname='freenode')
-penis.ircsocket.ircconn('irc.efnet.net', 6667, 'rxmirror', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#rapidxen' }, friendlyname='efnet')
+penis.ircsocket.ircconn('irc.staticbox.net', 6667, 'relaybot', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#unicorncastle' }, friendlyname='staticbox')
+penis.ircsocket.ircconn('irc.woomoo.org', 6667, 'relaybot', protocol_handler=rfc1459_client, channels={ 'yXnVlqV': '#woomoo' }, friendlyname='woomoo')
 
 asyncore.loop()
 
